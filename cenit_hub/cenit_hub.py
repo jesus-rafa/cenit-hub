30f5638c8fad09dcc7c24de2939200831b074d2e
rlopezro <70593750+jesus-rafa@users.noreply.github.com>
2024-08-31 20:54:33 -0600

    Update cenit_hub.py

--- cenit_hub/cenit_hub.py
+++ cenit_hub/cenit_hub.py
@@ -61,7 +61,8 @@ def view_requests():
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    #app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 
 # url: str = os.getenv('SUPABASE_URL')
